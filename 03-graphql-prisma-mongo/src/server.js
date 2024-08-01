8107e1e60f1548d07297768ffe8a843b74bd151c
Sumit Khandelwal <synergy2411@outlook.com>
2024-08-01 13:28:37 +0530

    user and post added

--- 03-graphql-prisma-mongo/src/server.js
+++ 03-graphql-prisma-mongo/src/server.js
@@ -15,9 +15,46 @@ async function main() {
 
   //   console.log("RESULT : ", result);
 
-  const allUsers = await prisma.user.findMany();
+  //   const allUsers = await prisma.user.findMany();
 
-  console.log(allUsers);
+  //   console.log(allUsers);
+
+  // POST CREATED
+
+  //   const result = await prisma.post.create({
+  //     data: {
+  //       title: "Advanced React",
+  //       body: "...",
+  //       published: true,
+  //       authorId: "66ab370ffc6018adc6802b7a",
+  //     },
+  //   });
+
+  //   console.log("RESULT : ", result);
+
+  //   const allUsers = await prisma.user.findMany({
+  //     include: {
+  //       Post: true,
+  //     },
+  //   });
+
+  //   console.log(allUsers);
+
+  const allPosts = await prisma.post.findMany({
+    include: {
+      author: true,
+    },
+    orderBy: {
+      title: "asc",
+    },
+    // take: 100,
+    // skip: 100,
+    // where: {
+    //   published: false,
+    // },
+  });
+
+  console.log(allPosts);
 }
 
 main()
@@ -26,3 +63,5 @@ main()
     prisma.$disconnect();
   })
   .finally(() => prisma.$disconnect());
+
+//   node filename.js

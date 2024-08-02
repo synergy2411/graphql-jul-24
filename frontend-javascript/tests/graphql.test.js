647129b42daf12c0ff1403b179c0e254fbc7681a
Sumit Khandelwal <synergy2411@outlook.com>
2024-08-02 11:06:21 +0530

    graphql tests added

---
+++ frontend-javascript/tests/graphql.test.js
@@ -0,0 +1,74 @@
+const {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  HttpLink,
+} = require("@apollo/client");
+const fetch = require("cross-fetch");
+
+describe("GraphQL Query", () => {
+  let client = null;
+
+  // Setup and Tear down
+  beforeEach(() => {
+    client = new ApolloClient({
+      link: new HttpLink({
+        fetch,
+        uri: "http://localhost:4000/graphql",
+      }),
+      cache: new InMemoryCache(),
+    });
+  });
+
+  afterEach(() => (client = null));
+
+  test("should fetch all the posts from GraphQL Server", async () => {
+    //   Arrange
+    const FETCH_POSTS = gql/* GraphQL */ `
+      query {
+        posts {
+          id
+          title
+          body
+          published
+        }
+      }
+    `;
+
+    //   Act
+    const { data } = await client.query({ query: FETCH_POSTS });
+
+    //   Assert
+    expect(data).not.toBeUndefined();
+    expect(data.posts.length).not.toEqual(0);
+    expect(data.posts.length).toEqual(2);
+  });
+
+  test("should retrun token when correct credentials are given", async () => {
+    const USER_LOGIN = gql`
+      mutation SignIn {
+        signIn(data: { email: "joey@test", password: "joey123" }) {
+          token
+        }
+      }
+    `;
+
+    const { data } = await client.mutate({ mutation: USER_LOGIN });
+
+    expect(data.signIn.token).not.toBeUndefined();
+  });
+
+  test.skip("should not retrun token when incorrect credentials are given", async () => {
+    const USER_LOGIN = gql`
+      mutation SignIn {
+        signIn(data: { email: "john@test", password: "john123" }) {
+          token
+        }
+      }
+    `;
+
+    const { data } = await client.mutate({ mutation: USER_LOGIN });
+
+    expect(data.signIn.token).toBeUndefined();
+  });
+});
